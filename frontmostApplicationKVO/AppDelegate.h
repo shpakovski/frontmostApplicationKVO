6eb6059d12d80c880d156a4986f88c53f6791ad3
Vadim Shpakovski <vadim@shpakovski.com>
2012-02-29 00:04:13 +0300

    Adds frontmost app tracking.

--- frontmostApplicationKVO/AppDelegate.h
+++ frontmostApplicationKVO/AppDelegate.h
@@ -1,15 +1,7 @@
-//
-//  AppDelegate.h
-//  frontmostApplicationKVO
-//
-//  Created by Вадим Шпаковский on 2/28/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
-
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
-@property (assign) IBOutlet NSWindow *window;
+@property (unsafe_unretained) IBOutlet NSWindow *window;
+@property (unsafe_unretained) IBOutlet NSTextView *textView;
+@property (nonatomic, getter = isObserving) BOOL observing;
 
 @end
--- frontmostApplicationKVO/AppDelegate.m
+++ frontmostApplicationKVO/AppDelegate.m
@@ -1,20 +1,70 @@
-//
-//  AppDelegate.m
-//  frontmostApplicationKVO
-//
-//  Created by Вадим Шпаковский on 2/28/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @implementation AppDelegate
 
 @synthesize window = _window;
+@synthesize textView = _textView;
+@synthesize observing = _observing;
+
+#pragma mark -
+
+- (void)dealloc
+{
+    [self stopObservingIfObserving];
+}
+
+#pragma mark -
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    self.observing = YES;
+}
+
+#pragma mark -
+
+- (void)setObserving:(BOOL)observing
+{
+    if (_observing != observing) {
+        [self stopObservingIfObserving];
+        _observing = observing;
+        [self startObservingIfObserving];
+    }
+}
+
+- (void)logMessage:(NSString *)message
+{
+    [self.textView replaceCharactersInRange:NSMakeRange(self.textView.textStorage.length, 0) withString:message];
+    [self.textView replaceCharactersInRange:NSMakeRange(self.textView.textStorage.length, 0) withString:@"\n"];
+    [self.textView scrollToEndOfDocument:nil];
+}
+
+void *kContextFrontmostApp = &kContextFrontmostApp;
+
+- (void)startObservingIfObserving
+{
+    if (_observing) {
+        [self logMessage:@"{"];
+        [[NSWorkspace sharedWorkspace] addObserver:self forKeyPath:@"frontmostApplication" options:NSKeyValueObservingOptionInitial context:kContextFrontmostApp];
+    }
+}
+
+- (void)stopObservingIfObserving
+{
+    if (_observing) {
+        [[NSWorkspace sharedWorkspace] removeObserver:self forKeyPath:@"frontmostApplication" context:kContextFrontmostApp];
+        [self logMessage:@"}\n"];
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if (context == kContextFrontmostApp) {
+        NSRunningApplication *frontmostApp = [object valueForKey:keyPath];
+        [self logMessage:[NSString stringWithFormat:@"\t%@;", frontmostApp.bundleIdentifier]];
+    }
+    else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
 }
 
 @end
